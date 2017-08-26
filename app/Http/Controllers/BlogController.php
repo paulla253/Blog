1bf798c361bc076a2ccd8dfc433c2b6a980e0291
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2017-08-26 09:48:33 -0300

    Controller mandando conteudo para a view

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -8,6 +8,24 @@ class BlogController extends Controller
 {
     public function index()
     {
-       return view('blog/index');
+        /*No futuro criar um model para organizar os conteudos*/
+
+        $titulo = [
+            0=>'Pokémon GO',
+            1=>'Clash Royale',
+            2=>'Minha Talking Angela',
+        ];
+
+        $conteudos = [
+            0=>'Pokémon GO é um jogo eletrônico free-to-play de realidade aumentada voltado para smartphones.
+               Foi desenvolvido por uma colaboração entre a Niantic, Inc.,
+               a Nintendo e a The Pokémon Company para as plataformas iOS e Android',
+            1=>'Clash Royale é um videojogo de estratégia freemium desenvolvido e publicado pela Supercell,
+                sendo um videojogo da empresa sediada em Helsínquia, na Finlândia.',
+            2=>'Explore o mundo da Talking Angela e personalize sua roupa, penteado,
+                maquiagem e casa – tudo enquanto joga minigames viciantes e fofinhos.',
+        ];
+
+       return view('blog/index',compact('titulos','conteudos'));
     }
 }
--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -3,36 +3,17 @@
 
 @section('conteudo')
 
-
-    {{--@foreach($notas as $nota)--}}
-
-    {{--<li>{{$nota}}</li>--}}
-
-    {{--@endforeach--}}
-
     <div class="row">
 
-        <div class="col-lg-4">
-            <h2>Pokémon GO</h2>
-            <p>Pokémon GO é um jogo eletrônico free-to-play de realidade aumentada voltado para smartphones.
-               Foi desenvolvido por uma colaboração entre a Niantic, Inc.,
-               a Nintendo e a The Pokémon Company para as plataformas iOS e Android</p>
-            <p><a class="btn btn-primary" href="#" role="button">Ver detalhes &raquo;</a></p>
-        </div>
+        @foreach($conteudos as $conteudo )
 
-        <div class="col-lg-4">
-            <h2>Clash Royale</h2>
-            <p>Clash Royale é um videojogo de estratégia freemium desenvolvido e publicado pela Supercell,
-                sendo um videojogo da empresa sediada em Helsínquia, na Finlândia.</p>
-            <p><a class="btn btn-primary" href="#" role="button">Ver detalhes &raquo;</a></p>
-        </div>
+         <div class="col-lg-4">
+            <p>{{$conteudo}}</p>
 
-        <div class="col-lg-4">
-            <h2>Minha Talking Angela</h2>
-            <p>Explore o mundo da Talking Angela e personalize sua roupa, penteado,
-                maquiagem e casa – tudo enquanto joga minigames viciantes e fofinhos.</p>
-            <p><a class="btn btn-primary" href="#" role="button">Ver detalhes &raquo;</a></p>
-        </div>
+             <p><a class="btn btn-primary" href="#" role="button">Ver detalhes &raquo;</a></p>
+         </div>
+
+        @endforeach
 
     </div>
 
