ce66d88a02cbb303ec796c53c38cf1f1bdba6dbf
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2017-08-28 21:36:36 -0300

    Criado os arquivos para models

---
+++ database/migrations/2017_08_29_000348_create_talbe_post.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTalbePost extends Migration
+{
+    /**
+     *Criará o esquema do banco
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title,255');
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * deletará o esquema do banco.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
