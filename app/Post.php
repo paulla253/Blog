a3474cc56bf7dde0f5680b11872440fa42a41d3b
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2017-08-29 18:28:10 -0300

    Criando o protected na model para inserir conteudos em massa

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    //Criar arquivo em massa utilizando por exemplo tinker;
+    //Exemplo : App\Post::create(['title'=>'Terceiro Post','content'=>'Conteudo do terceiro post'])
+    protected $fillable=[
+        'title',
+        'content'
+    ];
 }
