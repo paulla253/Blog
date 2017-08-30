cd80aa85a498953dde9f30cb2710f52b45e37e10
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2017-08-30 17:16:14 -0300

    Criando modelo e a migration do comment

--- database/migrations/2017_08_29_000348_create_posts_table.php
+++ database/migrations/2017_08_29_000348_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTalbePost extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      *Criará o esquema do banco
